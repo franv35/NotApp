7102da36335112147fb3cc6c34426c1d73085750
Joako <87424697+Nadharia@users.noreply.github.com>
2025-06-05 20:19:58 -0300

    cambios varios en note Entity

--- src/main/java/com/imb2025/notapp/entity/Etiqueta.java
+++ src/main/java/com/imb2025/notapp/entity/Etiqueta.java
@@ -14,7 +14,6 @@ public class Etiqueta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
-    @ManyToOne
-    @JoinColumn(name = "nota_id")
-    private Note nota;
+
+
 }
