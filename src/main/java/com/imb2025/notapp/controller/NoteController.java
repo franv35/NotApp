c1a573dfb722020cfc75fecbd525888a09a35d92
Joako <87424697+Nadharia@users.noreply.github.com>
2025-06-05 17:19:35 -0300

    cambios

--- src/main/java/com/imb2025/notapp/controller/NoteController.java
+++ src/main/java/com/imb2025/notapp/controller/NoteController.java
@@ -37,4 +37,12 @@ public class NoteController {
     public ResponseEntity<String> deleteById(@PathVariable Long id){
         return ResponseEntity.ok(iNotesService.deleteById(id));
     }
+    @PutMapping("/editartitle")
+    public ResponseEntity<String> updateTitle(@RequestBody Long id,String title){
+        return ResponseEntity.ok(iNotesService.updateTitle(id,title));
+    }
+    @PutMapping("/editarcontenido")
+    public ResponseEntity<String> updateContent(@RequestBody Long id,String content){
+        return ResponseEntity.ok(iNotesService.updateContent(id,content));
+    }
 }
