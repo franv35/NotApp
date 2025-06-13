db06badd2f74593a81d4dfc20bbc1bc8ea6f46a2
Joako <87424697+Nadharia@users.noreply.github.com>
2025-06-12 23:19:18 -0300

    cambios dtos PUT

--- src/main/java/com/imb2025/notapp/entity/dto/RegisterRequest.java
+++ src/main/java/com/imb2025/notapp/entity/dto/RegisterRequest.java
@@ -3,10 +3,12 @@ package com.imb2025.notapp.entity.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
     private String title;
     private String contenido;
